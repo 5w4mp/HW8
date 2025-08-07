e21df028333bfab48d8f2e69d0d037a11aa4ba71
Zach Timberlake <zachindeed@gmail.com>
2025-08-06 17:23:23 -0700

    Homework 8 for CSCD300

---
+++ src/LInkedList.java
@@ -0,0 +1,54 @@
+public class LInkedList
+{
+
+    private class Node
+    {
+        private Object data;
+        private Node prev, next;
+        private Node(Object data, Node pref, Node next)
+        {
+            this.data = data;
+            this.prev = pref;
+            this.next = next;
+        }
+    }
+    private Node head;
+    private int size;
+
+    public LInkedList()
+    {
+        this.head = new Node(null, null, null);
+        this.head.next = this.head;
+        this.head.prev = this.head;
+        this.size = 0;
+    }
+
+    public boolean isEmpty(){return this.head == this.head.next;}
+
+    public void addLast(Object data)
+    {
+        Node nn = new Node(data, this.head.prev, this.head);
+        this.head.prev.next = nn;
+        this.head.prev = nn;
+        this.size++;
+    }
+
+    public void removeFirst()
+    {
+        Node cur = this.head.next;
+        this.head.next = cur.next;
+        cur.next.prev = this.head;
+        this.size--;
+    }
+
+    /**
+     * Need:
+     * Merge sort goes into this class
+     * in
+     */
+    public class DoublyLinkedList
+    {
+
+    }
+
+}
---
+++ src/Tester.java
@@ -0,0 +1,6 @@
+import static org.junit.jupiter.api.Assertions.*;
+class Tester {
+    @Test
+    void setUp() {
+    }
+}
